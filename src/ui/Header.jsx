b969e8ac9057bbe85e335202fa010497d3352dde
ramonsales <ramon.silvasales@gmail.com>
2024-11-23 11:59:33 -0300

    Build the app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>eu e casca de bala</p>
+    </header>
+  );
+}
+
+export default Header;
