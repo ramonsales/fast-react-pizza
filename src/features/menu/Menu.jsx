1c130802c20a3b0cb9cbd281caa7dba4442e5630
ramonsales <ramon.silvasales@gmail.com>
2024-11-25 10:05:27 -0300

    Add styling to the application

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,17 +1,16 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
   const menu = useLoaderData();
+
   return (
-    <div>
-      <ul>
-        {menu.map((pizza) => (
-          <MenuItem key={pizza.id} pizza={pizza} />
-        ))}
-      </ul>
-    </div>
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
   );
 }
 
